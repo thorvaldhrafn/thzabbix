3934ccb12a99867b126dc4cce184cb8402dc77ca
thorvald <thorvaldr.hrafn@gmail.com>
2019-05-17 14:12:19 +0300

    First

--- webcheck.py
+++ webcheck.py
@@ -37,6 +37,10 @@ for hst in ans_hlist:
 
 for anshst, h_ip in ans_hlist_ips.iteritems():
     shell_comm = "bash /usr/local/thscripts/bin/ths-list-domains.sh"
+    result = ansshell(shell_comm, anshst)
+    if type(result) is list:
+        print(result[1])
+        break
     print(h_ip, ansshell(shell_comm, anshst))
 
 print("End")
