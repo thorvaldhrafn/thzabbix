e05fad7ce7d3def0521a13f3132d63871e38301b
thorvald <thorvaldr.hrafn@gmail.com>
2019-05-17 15:12:14 +0300

    First

--- functions.py
+++ functions.py
@@ -116,7 +116,7 @@ def ansshell(comm, hst):
         value = re.sub('\n +\n', '\n', value, count=1)
         value = value.rstrip('\n')
     except subprocess.CalledProcessError, e:
-        return "Error", e.output
+        return list("Error", e.output)
     return value
 
 
