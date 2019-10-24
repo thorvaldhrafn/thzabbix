a47df58ac5e05834290bcdba52479fa559f02b30
thorvald <thorvald@gridinsoft.com>
2019-10-24 14:19:19 +0300

    First

--- httptest.py
+++ httptest.py
@@ -6,30 +6,30 @@ from functions import headers, url, authtock, hostidbyip
 
 class HTTPtest(object):
     def __init__(self):
-        self.test_delay = str("3m")
-        self.test_timeout = str("30s")
-        self.test_redir = str("0")
+        self.delay = str("3m")
+        self.timeout = str("30s")
+        self.follow_redirects = str("0")
 
     def _addparam(self, add_params):
         try:
-            add_params["test_delay"]
+            add_params["delay"]
         except KeyError:
-            add_params["test_delay"] = self.test_delay
+            add_params["delay"] = self.delay
         try:
-            add_params["test_timeout"]
+            add_params["timeout"]
         except KeyError:
-            add_params["test_timeout"] = self.test_timeout
+            add_params["timeout"] = self.timeout
         try:
-            add_params["test_redir"]
+            add_params["follow_redirects"]
         except KeyError:
-            add_params["test_redir"] = self.test_redir
+            add_params["follow_redirects"] = self.follow_redirects
         return add_params
 
     def httptest_addlist(self, add_params):
         add_params = self._addparam(add_params)
         host_id = hostidbyip(add_params["host_ip"])
-        full_params = {"hostid": host_id, "delay": add_params["test_delay"], "name": add_params["domain"], "retries": str("3")}
-        steps_dict = {"name": add_params["domain"], "url": add_params["url"], "follow_redirects": add_params["test_redir"], "timeout": add_params["test_timeout"], "status_codes": add_params["test_code"], "no": str("1")}
+        full_params = {"hostid": host_id, "delay": add_params["delay"], "name": add_params["name"], "retries": str("3")}
+        steps_dict = {"name": add_params["domain"], "url": add_params["url"], "follow_redirects": add_params["follow_redirects"], "timeout": add_params["timeout"], "status_codes": add_params["status_codes"], "no": str("1")}
         steps_list = list()
         steps_list.append(steps_dict)
         full_params["steps"] = steps_list
@@ -38,11 +38,12 @@ class HTTPtest(object):
     def httptest_updlist(self, upd_params):
         upd_params = self._addparam(upd_params)
         full_params = {"httptestid": upd_params["httptestid"], "delay": upd_params["test_delay"]}
-        steps_dict = {"follow_redirects": upd_params["test_redir"], "name": upd_params["domain"], "url": upd_params["url"], "timeout": upd_params["test_timeout"], "status_codes": upd_params["code"], "no": str("1")}
+        steps_dict = {"follow_redirects": upd_params["follow_redirects"], "name": upd_params["name"], "url": upd_params["url"], "timeout": upd_params["timeout"], "status_codes": upd_params["status_codes"], "no": str("1")}
         steps_list = list()
         steps_list.append(steps_dict)
         full_params["steps"] = steps_list
         return full_params
+
 
     def httptestdel(self, httptestid):
         paramslst = {"httptestid": httptestid}
