377672551c795d45f1d8c9361cb7e855746c38a7
thorvald <thorvald@gridinsoft.com>
2021-11-11 18:26:51 +0200

    2to3

--- httptest.py
+++ httptest.py
@@ -76,6 +76,7 @@ class HTTPtest(object):
         test_add_data = self.basedata.copy()
         test_add_data["params"] = params
         test_add_data["method"] = method
+        print(test_add_data)
         httptestret = self._req_post(test_add_data).json()
         result = list()
         result.append(httptestret)
