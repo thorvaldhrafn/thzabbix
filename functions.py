e2bd332c422f54d0aade2a45b08ca526d2c2bbe9
thorvald <thorvald@gridinsoft.com>
2021-11-18 13:47:26 +0200

    Big cleaning

--- functions.py
+++ functions.py
@@ -58,7 +58,7 @@ class ZabbReq(object):
         paramslst = dict(output="extend")
         hosts_get = dict(method="host.get", data=paramslst)
         hosts_get_data = self.req_post(hosts_get)["result"]
-        return hosts_get_data[0]
+        return hosts_get_data[1]
 
 
 # def conf_get(conf_file):
