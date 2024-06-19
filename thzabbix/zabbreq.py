5101726ffc671873d41f6ed27ddd60a88f0b6b15
thorv <thorvaldr.hrafn@outlook.com>
2024-06-19 16:29:52 +0300

    PyPi release 0.1.3 Master.

--- thzabbix/zabbreq.py
+++ thzabbix/zabbreq.py
@@ -63,7 +63,7 @@ class ZabbReq:
 
     def host_tmplt_upd(self, hostid, tmplt_lst):
         hupdttedata = dict(hostid=hostid)
-        tmplt_lst=list(dict.fromkeys(tmplt_lst))
+        tmplt_lst = list(dict.fromkeys(tmplt_lst))
         hupdttedata["templates"] = tmplt_lst
         updtmplt_req = dict(method="host.update", params=hupdttedata)
         return self.req_post(updtmplt_req)
